9b456219454176594541b1793763f430257fdb3b
Areeje <43833530+Areeje@users.noreply.github.com>
2018-10-05 16:28:14 -0700

    Update solution1.sh

--- solution1.sh
+++ solution1.sh
@@ -9,9 +9,9 @@ $ gunzip Ecoli-vs-Yersinia.BLASTP.tab.gz
 $ du Ecoli-vs-Yersinia.BLASTP.tab
 $ head -25 Ecoli-vs-Yersinia.BLASTP.tab
 $ tail -3 Ecoli-vs-Yersinia.BLASTP.tab
-wc -l Ecoli-vs-Yersinia.BLASTP.tab
-curl -O https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.taz
-awk '{print ($1,$6)}' Nc3H.expr.tab > Nc20H.expr.sorted.tab
-sort -nrk 2 Nc20H.expr.sorted.tab | head -10
+$wc -l Ecoli-vs-Yersinia.BLASTP.tab
+$curl -O https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.taz
+$awk '{print ($1,$6)}' Nc3H.expr.tab > Nc20H.expr.sorted.tab
+$sort -nrk 2 Nc20H.expr.sorted.tab | head -10
 $ curl -O https://biodataprog.github.io/2018_programming-intro/data/D_mel.63B12.gbk
 $ grep -c CDS D_mel.63B12.gbk
