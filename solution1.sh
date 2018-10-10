1acec658cbd6ed229ab8cf24ee01ca6acdb822d7
Areeje <43833530+Areeje@users.noreply.github.com>
2018-10-10 01:44:46 -0700

    Update solution1.sh

--- solution1.sh
+++ solution1.sh
@@ -7,9 +7,7 @@ $curl -O https://biodataprog.github.io/2018_programming-intro/data/Ecoli-vs-Yers
 $ du Ecoli-vs-Yersinia.BLASTP.tab.gz
 $ gunzip Ecoli-vs-Yersinia.BLASTP.tab.gz
 $ du Ecoli-vs-Yersinia.BLASTP.tab
-$ head -25 Ecoli-vs-Yersinia.BLASTP.tab
-$ tail -3 Ecoli-vs-Yersinia.BLASTP.tab
-$ wc -l Ecoli-vs-Yersinia.BLASTP.tab
+
 
 
 
